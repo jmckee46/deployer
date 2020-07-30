5e4b1a3ff5733d04173e63a6b6fb06c9cae50e0c
John McKee <jmckee3@mac.com>
2020-07-29 17:03:52 -0700

    render-stack-template writes to file

--- aws/render-stack-template.go
+++ aws/render-stack-template.go
@@ -1,7 +1,9 @@
 package awsfuncs
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/alecthomas/template"
 	"github.com/jmckee46/deployer/flaw"
@@ -10,18 +12,38 @@ import (
 
 // RenderStackTemplate assembles the various stack templates into one
 func RenderStackTemplate() flaw.Flaw {
+	// make new branch directory in artifacts
+	branchName := filepath.Join(
+		os.Getenv("DE_ARTIFACTS_PATH"),
+		os.Getenv("DE_STACK_NAME"),
+	)
+
+	err := os.MkdirAll(branchName, 0755)
+	if err != nil {
+		return flaw.From(err)
+	}
+
+	// create file
+	newStackFileName := filepath.Join(
+		branchName,
+		"completeStack",
+	)
+	newStackFile, err := os.Create(newStackFileName)
+	if err != nil {
+		fmt.Println("trouble creating file:", err)
+	}
+
 	branchStackTemplate := template.New("root.gotemplate")
 
 	branchStackTemplate.Delims("{{{", "}}}")
 
-	_, err :=
-		branchStackTemplate.ParseGlob("aws/stack-templates/*.gotemplate")
+	_, err = branchStackTemplate.ParseGlob("aws/stack-templates/*.gotemplate")
 
 	if err != nil {
 		halt.Panic(flaw.From(err))
 	}
 
-	err = branchStackTemplate.Execute(os.Stdout, nil)
+	err = branchStackTemplate.Execute(newStackFile, nil)
 
 	if err != nil {
 		halt.Panic(flaw.From(err))
